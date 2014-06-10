8fc3bc2f0f0e979bdb00bc8ae7a71f0e56dc04e0
friflaj <friflaj@ajellito.com>
2014-06-10 14:42:14 +0200

    sumatrapdf

---
+++ lib/macaw/rules/sumatrapdf.rb
@@ -0,0 +1,10 @@
+require 'process'
+# sumatrapdf rule for arara
+class Macaw
+  def sumatrapdf
+    Macaw.log('spawning sumatrapdf...')
+
+    pdf = ~"#{@base}.pdf"
+    Process.spawn "sumatrapdf -reuse-instance #{pdf}"
+  end
+end
